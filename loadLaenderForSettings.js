dbd71ab57cc6a4f48a80dcfc3506e973c5895049
svchoster <62jama1bif@hft-stuttgart.de>
2017-06-15 13:50:06 +0200

    javaScript funktioniert noch nicht

--- loadLaenderForSettings.js
+++ loadLaenderForSettings.js
@@ -2,18 +2,13 @@
  * Created by jaik on 14.06.17.
  */
 
-function showProposal() {
-    if (str.length == 0) {
-        document.getElementById("txtProposal").innerHTML = "";
-        return;
-    } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("testLaender").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET", "WM.php, true);
-        xmlhttp.send();
-    }
-};
+function showlaender() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = JSON.parse(this.responseText);
+            document.getElementById("testLaender").innerHTML = myObj[2];
+        }
+    };
+    xmlhttp.open("GET", "WM.php", true);
+    xmlhttp.send();
