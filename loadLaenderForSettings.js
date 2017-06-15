0bd06ef100aea8263df13255a36efcb39267a4b7
svchoster <62jama1bif@hft-stuttgart.de>
2017-06-15 17:20:57 +0200

    Funktioniert jetzt mit der WM.php.
    Muss zum richtigen Gebrauch geändert werden

--- loadLaenderForSettings.js
+++ loadLaenderForSettings.js
@@ -4,11 +4,17 @@
 
 function showlaender() {
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
+
+    xmlhttp.onreadystatechange = function () {
+        var s = "";
         if (this.readyState == 4 && this.status == 200) {
-            var myObj = JSON.parse(this.responseText);
-            document.getElementById("testLaender").innerHTML = myObj[2];
+            myObj = JSON.parse(this.response);
+            for (i = 0; i < myObj.length; i++) {
+                s += myObj[i].id + ' ' + myObj[i].name + "</br>";
+            }
+            document.getElementById("demo").innerHTML = s;
         }
     };
     xmlhttp.open("GET", "WM.php", true);
     xmlhttp.send();
+}
