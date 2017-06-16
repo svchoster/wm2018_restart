f309142891488d2002d4d86d5292287ba8f711b5
svchoster <62jama1bif@hft-stuttgart.de>
2017-06-16 21:46:46 +0200

    index.html und loadLaenderForSettings.js angepasst. css background image angepasst

--- loadLaenderForSettings.js
+++ loadLaenderForSettings.js
@@ -5,14 +5,57 @@
 function showlaender() {
     var xmlhttp = new XMLHttpRequest();
 
-    xmlhttp.onreadystatechange = function () {
-        var s = "";
+    xmlhttp.onreadystatechange = function() {
+
+        var a_gruppen = new Array();
+
         if (this.readyState == 4 && this.status == 200) {
-            myObj = JSON.parse(this.response);
-            for (i = 0; i < myObj.length; i++) {
-                s += myObj[i].id + ' ' + myObj[i].name + "</br>";
+            myObj = JSON.parse(this.response); //Code parsen
+            laenderNr = 0;
+            gruppenNr = 0;
+            a_gruppen[gruppenNr] = new Array();
+            for(i = 0; i < myObj.length; i++){
+                a_gruppen[gruppenNr][laenderNr] = myObj[i];
+                if(laenderNr == 3){
+                    laenderNr = 0;
+                    gruppenNr++;
+                    a_gruppen[gruppenNr] = new Array();
+                }else {
+                    laenderNr++;
+                }
             }
-            document.getElementById("demo").innerHTML = s;
+
+            //Ausgeben in Tabelle
+            var s2 = "";
+
+            for(i = 0; i < a_gruppen.length; i++){
+                var a_laender = a_gruppen[i];
+
+                s2 += "<h2>Gruppe " + (i+1) + "</h2> " +
+                    '<table class="table table-hover">' +
+                    "<thead>" +
+                    "<tr>" +
+                    "<th>Nummer</th>" +
+                    "<th>Land</th>" +
+                    "</tr>" +
+                    "</thead>";
+
+                for(j = 0; j < a_laender.length; j++){
+                    obj_land = a_gruppen[i][j];
+
+                    s2 += "<tr>" +
+                        "<td>" + (j+1) + "</td>" +
+                        "<td>" + obj_land.name + "</td>" +
+                        "</tr>";
+
+                }
+                s2 += "<tbody>" +
+                    "</tbody>" +
+                    "</table>";
+            }
+
+            document.getElementById("tabellen").innerHTML = s2;
+
         }
     };
     xmlhttp.open("GET", "WM.php", true);
