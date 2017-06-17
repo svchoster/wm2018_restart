2694cb51c989abecd6e9e2f9c38bcb57410221c0
pleasurey <yannickhafner@gmx.de>
2017-06-17 17:02:48 +0200

    Aktualisiert 17.06.2017

--- loadLaenderForSettings.js
+++ loadLaenderForSettings.js
@@ -1,65 +1,64 @@
-/**
- * Created by jaik on 14.06.17.
- */
+var xmlhttp = new XMLHttpRequest();
+var a_gruppenObjekte;
 
-function showlaender() {
-    var a_gruppenGlobal;
-    var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
 
-    xmlhttp.onreadystatechange = function() {
+    var a_gruppen = new Array();
 
-        var a_gruppen = new Array(); // test
-
-        if (this.readyState == 4 && this.status == 200) {
-            myObj = JSON.parse(this.response); //Code parsen
-            laenderNr = 0;
-            gruppenNr = 0;
-            a_gruppen[gruppenNr] = new Array();
-            for(i = 0; i < myObj.length; i++){
-                a_gruppen[gruppenNr][laenderNr] = myObj[i];
-                if(laenderNr == 3){
-                    laenderNr = 0;
-                    gruppenNr++;
+    if (this.readyState == 4 && this.status == 200) {
+        myObj = JSON.parse(this.response); //Code parsen
+        laenderNr = 0;
+        gruppenNr = 0;
+        a_gruppen[gruppenNr] = new Array();
+        for(i = 0; i < myObj.length; i++){
+            a_gruppen[gruppenNr][laenderNr] = myObj[i];
+            if(laenderNr == 3) {
+                laenderNr = 0;
+                gruppenNr++;
+                if (i != myObj.length - 1) {
                     a_gruppen[gruppenNr] = new Array();
-                }else {
-                    laenderNr++;
                 }
+            }else {
+                laenderNr++;
             }
-            a_gruppenGlobal = a_gruppen;
-
-            //Ausgeben in Tabelle
-            var s2 = "";
-
-            for(i = 0; i < a_gruppen.length; i++){
-                var a_laender = a_gruppen[i];
-
-                s2 += "<h2>Gruppe " + (i+1) + "</h2> " +
-                    '<table class="table table-hover">' +
-                    "<thead>" +
-                    "<tr>" +
-                    "<th>Nummer</th>" +
-                    "<th>Land</th>" +
-                    "</tr>" +
-                    "</thead>";
-
-                for(j = 0; j < a_laender.length; j++){
-                    obj_land = a_gruppen[i][j];
-
-                    s2 += "<tr>" +
-                        "<td>" + (j+1) + "</td>" +
-                        "<td>" + obj_land.name + "</td>" +
-                        "</tr>";
-
-                }
-                s2 += "<tbody>" +
-                    "</tbody>" +
-                    "</table>";
-            }
-
-            document.getElementById("tabellen").innerHTML = s2;
-
         }
-    };
-    xmlhttp.open("GET", "WM.php", true);
-    xmlhttp.send();
-}
+
+        //Ausgeben in Tabelle
+        var outputString = "";
+        a_gruppenObjekte = a_gruppen;
+
+        for(i = 0; i < a_gruppenObjekte.length; i++){
+            var a_laender = a_gruppenObjekte[i];
+
+            outputString += "<h2>Gruppe " + (i+1) + "</h2> " +
+                '<table class="table table-bordered">' +
+                "<thead>" +
+                "<tr>" +
+                "<th width='50%'>Nummer</th>" +
+                "<th width='50%'>Land</th>" +
+                "</tr>" +
+                "</thead>" +
+                "<tbody>";
+
+            for(j = 0; j < a_laender.length; j++){
+                obj_land = a_gruppenObjekte[i][j];
+
+                outputString += "<tr>" +
+                    "<td width='50%'>" + (j+1) + "</td>" +
+                    "<td width='50%'>" +
+                    "<img border='0' alt='" +obj_land.flaggs+ "' src='Flags/" + obj_land.flaggs + ".png' width='25'> " + obj_land.name + "</td>" +
+                    "</tr>";
+
+            }
+            outputString += "</tbody>" +
+                "</table>";
+        }
+
+        document.getElementById("tabellen").innerHTML = outputString;
+
+    }
+};
+xmlhttp.open("GET", "WM.php", true);
+xmlhttp.send();
+
+
